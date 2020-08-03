6f798cff057f531aaa2d3e7bbc1485b796e26acb
Romain Beucher <romain.beucher@anu.edu.au>
2020-08-03 16:19:27 +1000

    work on conda build again

--- conda/build.sh
+++ conda/build.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+if [[ $(uname) == Linux ]]; then
+    export LDFLAGS="$LDFLAGS -Wl,-rpath-link,$PREFIX/lib"
+fi
+
+
+
 # test
-cd underworld/libUnderworld
-export PETSC_DIR=$PREFIX
-./configure.py --prefix=$PREFIX/lib/python*/site-packages/ || (cat config.log && ls /lib exit 1)
-./compile.py
-./scons.py install 
+python -m pip install -vvv --no-deps --ignore-installed --prefix=$PREFIX .
+#cd underworld/libUnderworld
+#export PETSC_DIR=$PREFIX
+#./configure.py --prefix=$PREFIX/lib/python*/site-packages/ || (cat config.log && ls /lib exit 1)
+#./compile.py
+#./scons.py install 
